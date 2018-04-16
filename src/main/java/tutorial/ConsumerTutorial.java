69b847b80b43df8cc73835f0d330bef2a4129cf6
Luc Perkins <lucperkins@gmail.com>
2018-04-16 16:42:01 -0700

    add earliest and latest examples for reader

--- src/main/java/tutorial/ConsumerTutorial.java
+++ src/main/java/tutorial/ConsumerTutorial.java
@@ -22,11 +22,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ConsumerTutorial {
-
+    private static final Logger log = LoggerFactory.getLogger(ConsumerTutorial.class);
     private static final String SERVICE_URL = "pulsar://localhost:6650";
-
-    private static final String TOPIC_NAME = "tutorial-topic";
-
+    private static final String TOPIC_NAME = "persistent://sample/standalone/ns1/tutorial-topic";
     private static final String SUBSCRIPTION_NAME = "tutorial-subscription";
 
     public static void main(String[] args) throws IOException {
@@ -46,7 +44,7 @@ public class ConsumerTutorial {
 
 
         // Once the consumer is created, it can be used for the entire application life-cycle
-        Consumer<byte[]> consumer = consumerBuilder.subscribe();    
+        Consumer<byte[]> consumer = consumerBuilder.subscribe();
         log.info("Created Pulsar consumer");
 
         while (true) {
@@ -61,7 +59,4 @@ public class ConsumerTutorial {
             consumer.acknowledge(msg);
         }
     }
-
-    private static final Logger log = LoggerFactory.getLogger(ProducerTutorial.class);
-
 }
---
+++ src/main/java/tutorial/ReaderTutorial.java
@@ -0,0 +1,59 @@
+package tutorial;
+
+import org.apache.pulsar.client.api.Message;
+import org.apache.pulsar.client.api.MessageId;
+import org.apache.pulsar.client.api.PulsarClient;
+import org.apache.pulsar.client.api.Reader;
+import org.apache.pulsar.client.api.ReaderBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class ReaderTutorial {
+    private static final Logger log = LoggerFactory.getLogger(ReaderTutorial.class);
+    private static final String SERVICE_URL = "pulsar://localhost:6650";
+    private static final String TOPIC_NAME = "persistent://sample/standalone/ns1/tutorial-topic";
+
+    public static void main(String[] args) throws IOException {
+        PulsarClient client = PulsarClient.builder()
+                .serviceUrl(SERVICE_URL)
+                .build();
+
+        ReaderBuilder<byte[]> readerBuilder = client.newReader()
+                .topic(TOPIC_NAME);
+
+        Reader<byte[]> earliestReader = readerBuilder
+                .startMessageId(MessageId.earliest)
+                .create();
+
+        Reader<byte[]> latestReader = readerBuilder
+                .startMessageId(MessageId.latest)
+                .create();
+
+        log.info("Reading messages from earliest ({})", MessageId.earliest);
+        do {
+            Message<byte[]> msg = earliestReader.readNext();
+            log.info("Message received with ID {}", msg.getMessageId());
+
+            if (earliestReader.hasReachedEndOfTopic()) {
+                log.info("Done reading from earliest");
+                earliestReader.close();
+            }
+        } while (!earliestReader.hasReachedEndOfTopic());
+
+
+        log.info("Reading messages from latest ({})", MessageId.latest);
+        do {
+            Message<byte[]> msg = latestReader.readNext();
+            log.info("Message received with ID {}", msg.getMessageId());
+
+            if (latestReader.hasReachedEndOfTopic()) {
+                log.info("Done reading from latest");
+                latestReader.close();
+            }
+        } while (!latestReader.hasReachedEndOfTopic());
+
+        client.close();
+    }
+}
